51386eb03670e7707fba7beb0891ccaf48e9b6b9
임하은 <gkdmsdl100414@gmail.com>
2026-02-19 15:55:20 +0900

    initial commit

---
+++ src/main/java/com/example/commerce/global/common/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.example.commerce.global.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    // JPA Auditing 용 추상화 Entity
+    // 생성일과 수정일
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
