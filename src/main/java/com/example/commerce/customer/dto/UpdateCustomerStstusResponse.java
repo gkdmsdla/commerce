5df22dcafde2e47b56fafae0822ea833c67aad98
kwonjiwon <kjw81024@gmail.com>
2026-02-23 20:52:05 +0900

    refactor(customer): ErrorCode featured, Service refactor with encoding

--- src/main/java/com/example/commerce/customer/dto/UpdateCustomerStstusResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.commerce.customer.dto;
-
-import lombok.Getter;
-
-import java.time.LocalDateTime;
-
-public record UpdateCustomerStstusResponse(Long id, String statusName, LocalDateTime modifiedAt) {
-}
